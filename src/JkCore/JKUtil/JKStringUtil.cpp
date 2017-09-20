4dad048f65a1f945b98f47c26d9f1325969811e6
wangjl-f <WANGJL-F@glodon.com>
2017-09-20 16:09:44 +0800

    添加字符编码

--- src/JkCore/JKUtil/JKStringUtil.cpp
+++ src/JkCore/JKUtil/JKStringUtil.cpp
@@ -1,7 +1,118 @@
 #include "JKStringUtil.h"
 #include <algorithm>
+#include <Windows.h>
 
 using namespace JK_NAMESPACE;
+
+wstring JKStringUtil::UTF8ToUnicode(const string& str)
+{
+	int  len = 0;
+	len = str.length();
+	int  unicodeLen = ::MultiByteToWideChar(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0);
+	wchar_t *  pUnicode;
+	pUnicode = new  wchar_t[unicodeLen + 1];
+	memset(pUnicode, 0, (unicodeLen + 1) * sizeof(wchar_t));
+	::MultiByteToWideChar(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		(LPWSTR)pUnicode,
+		unicodeLen);
+	wstring  rt;
+	rt = (wchar_t*)pUnicode;
+	delete  pUnicode;
+
+	return  rt;
+}
+
+string JKStringUtil::UnicodeToUTF8(const wstring& str)
+{
+	char*     pElementText;
+	int    iTextLen;
+	// wide char to multi char
+	iTextLen = WideCharToMultiByte(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0,
+		NULL,
+		NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+	::WideCharToMultiByte(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		pElementText,
+		iTextLen,
+		NULL,
+		NULL);
+	string strText;
+	strText = pElementText;
+	delete[] pElementText;
+	return strText;
+}
+
+wstring JKStringUtil::ANSIToUnicode(const string& str)
+{
+	int len = 0;
+	len = str.length();
+	int unicodeLen = ::MultiByteToWideChar(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0);
+	wchar_t * pUnicode;
+	pUnicode = new wchar_t[unicodeLen + 1];
+	memset(pUnicode, 0, (unicodeLen + 1) * sizeof(wchar_t));
+	::MultiByteToWideChar(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		(LPWSTR)pUnicode,
+		unicodeLen);
+	wstring rt;
+	rt = (wchar_t*)pUnicode;
+	delete pUnicode;
+	return rt;
+}
+
+
+string JKStringUtil::UnicodeToANSI(const wstring& str)
+{
+	char*     pElementText;
+	int    iTextLen;
+	// wide char to multi char
+	iTextLen = WideCharToMultiByte(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0,
+		NULL,
+		NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+	::WideCharToMultiByte(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		pElementText,
+		iTextLen,
+		NULL,
+		NULL);
+	string strText;
+	strText = pElementText;
+	delete[] pElementText;
+	return strText;
+}
 
 bool JKStringUtil::to_bool( string value )
 {
